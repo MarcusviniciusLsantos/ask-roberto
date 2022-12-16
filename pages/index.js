ddf39175e0f147322de4a97b4ca2abec72035602
Marcus Vinícius <marcusvinici25@gmail.com>
2022-12-16 00:33:34 -0300

    adjusting index

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,20 @@
 import Head from "next/head";
-import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MainNav from "../components/MainNav";
+import ChatPage from "../components/chat-page";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
-
-  async function onSubmit(event) {
-    event.preventDefault();
-    const response = await fetch("/api/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ animal: animalInput }),
-    });
-    const data = await response.json();
-    setResult(data.result);
-    setAnimalInput("");
-  }
-
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>Ask Roberto</title>
+        <link rel="icon" href="/roberto.png" />
       </Head>
-
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
+      <main>
+        <div style={{ width: "97vw" }}>
+          <ChatPage />
+        </div>
       </main>
     </div>
   );
