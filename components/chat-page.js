8afdf69ddba52e8a36295241fe3169592556e050
Marcus Vinícius <marcusvinici25@gmail.com>
2022-12-16 10:24:45 -0300

    adding isMobile preview

--- components/chat-page.js
+++ components/chat-page.js
@@ -6,6 +6,7 @@ import Avatar from "./avatar";
 import DocExamples from "./doc-examples";
 import Chat from "./chat";
 import Link from "next/link";
+import {isMobile} from "react-device-detect";
 
 export default function ChatPage() {
   const [text, setText] = useState("");
@@ -51,24 +52,39 @@ export default function ChatPage() {
   return (
     <Row>
       <Col xs="3" sm="3" md="3">
-        {console.log('results', results)}
-        <div className={styles.menu}>
-          <div>
-            <div className={styles.item}><Avatar name={"Roberto"} width={34} height={34} className="ms-3" title={false}/></div>
+        {console.log('results', results)} 
+        {!isMobile && (
+          <div className={styles.menu}>
+            <div>
+              <div className={styles.item}>
+                <Avatar
+                  name={"Roberto"}
+                  width={34}
+                  height={34}
+                  className="ms-3"
+                  title={false}
+                />
+              </div>
+            </div>
+            <div>
+              <div className={styles.divisor} />
+              <div className={styles.endItem}>
+                <Link href="https://dindgeri.github.io/askroberto/">
+                  <a
+                    className="text-decoration-none"
+                    style={{ color: "white" }}
+                  >
+                    <b>Back </b>
+                  </a>
+                </Link>
+              </div>
+            </div>
           </div>
-          <div>
-            <div className={styles.divisor} />
-            <div className={styles.endItem} ><Link href="https://dindgeri.github.io/askroberto/">
-              <a className="text-decoration-none" style={{color: "white"}}>
-              <b>Back </b>
-              </a>
-            </Link></div>
-          </div>
-        </div>
+        )}
       </Col>
       <Col xs="9" sm="9" md="9" className="d-flex justify-content-center">
         <div className="d-flex flex-column justify-content-between ">
-          <div className="d-flex justify-content-start">
+          <div className={`d-flex ${isMobile ? 'justify-content-center':'justify-content-start' }`}>
             <Avatar name={"Ask Roberto"} loading={loading}/>
           </div>
           <div>
