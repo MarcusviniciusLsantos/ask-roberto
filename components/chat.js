8e349ef6de97b76039f729141934975022ed61f2
Marcus Vinícius <marcusvinici25@gmail.com>
2022-12-16 00:33:03 -0300

    creating chat component

---
+++ components/chat.js
@@ -0,0 +1,20 @@
+import Avatar from "./avatar";
+import Card from "./card";
+
+export default function Chat({ text, reverse = false }) {
+  return (
+    <div className="d-flex flex-row justify-content-start">
+      {reverse ? (
+        <>
+          <Card name={text} />
+          <img src="https://img.pizza/34/34" width={34} height={34} style={{borderRadius: "16px"}} />
+        </>
+      ) : (
+        <>
+          <img src="/roberto.png" width={34} height={34} />
+          <Card name={text} />
+        </>
+      )}
+    </div>
+  );
+}
